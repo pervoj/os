13665c8db4cff5010b8af9b9c40a00d1393b68bd
Vojtěch Perník <develop@pervoj.cz>
2023-08-11 11:19:08 +0200

    Add prompt package

---
+++ packages/recipes/shell/prompt.sh
@@ -0,0 +1,21 @@
+NAME="prompt"
+VERSION="1.0.0"
+
+COMMIT=ecd76d34978e21d77d6627cf0038d588efaf7c04
+
+BUILD_DEPENDENCIES=(
+  "wget"
+)
+
+prepare() {
+  log "Downloading $NAME.sh."
+  wget \
+    -O $NAME.sh \
+    https://gitlab.com/pervoj/dotfiles/raw/$COMMIT/.bashrc.d/prompt.sh
+}
+
+install() {
+  log "Installing /etc/profile.d/$NAME.sh file."
+  mkdir -p $OUTPUT_DIR$PREFIX/etc/profile.d
+  mv $NAME.sh $OUTPUT_DIR$PREFIX/etc/profile.d
+}
