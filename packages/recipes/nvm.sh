5972c90edd71b38fed4a35eb537ddc5e53966f73
Vojtěch Perník <develop@pervoj.cz>
2023-08-11 11:03:19 +0200

    Migrate nvm package to the new features

--- packages/recipes/nvm.sh
+++ packages/recipes/nvm.sh
@@ -6,21 +6,21 @@ BUILD_DEPENDENCIES=(
 )
 
 prepare() {
-  git clone https://github.com/nvm-sh/nvm.git $BUILD_DIR/repo
-  cd $BUILD_DIR/repo
-  git checkout v$VERSION
-  cd ..
+  git_clone_tag https://github.com/nvm-sh/nvm.git v$VERSION
 }
 
 build() {
-  echo "source $PREFIX/share/$NAME/nvm.sh" > $BUILD_DIR/$NAME.sh
-  echo "source $PREFIX/share/$NAME/bash_completion" >> $BUILD_DIR/$NAME.sh
+  log "Creating /etc/profile.d/$NAME.sh file."
+  echo "source $PREFIX/share/$NAME/nvm.sh" > $NAME.sh
+  echo "source $PREFIX/share/$NAME/bash_completion" >> $NAME.sh
 }
 
 install() {
+  log "Installing $NAME source files."
   mkdir -p $OUTPUT_DIR$PREFIX/share
-  mv $BUILD_DIR/repo $OUTPUT_DIR$PREFIX/share/$NAME
+  mv repo $OUTPUT_DIR$PREFIX/share/$NAME
 
+  log "Installing /etc/profile.d/$NAME.sh file."
   mkdir -p $OUTPUT_DIR$PREFIX/etc/profile.d
-  mv $BUILD_DIR/$NAME.sh $OUTPUT_DIR$PREFIX/etc/profile.d
+  mv $NAME.sh $OUTPUT_DIR$PREFIX/etc/profile.d
 }
