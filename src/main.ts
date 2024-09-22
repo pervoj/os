42f5e62065ce34a037c47fce8a556fa5bb99c455
Vojtěch Perník <develop@pervoj.cz>
2024-09-22 19:21:39 +0200

    feat: add docker repo

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,4 @@
-console.log("Hello via Bun!");
+import { addRepository } from "./utils/repos";
+
+console.log("Adding Docker Repository...");
+await addRepository("https://download.docker.com/linux/fedora/docker-ce.repo");
