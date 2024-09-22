a6095ddbf9ee0fd2136d95197390caca202b9d01
Vojtěch Perník <develop@pervoj.cz>
2024-09-22 19:58:02 +0200

    feat: add logging to repo util

--- src/utils/repos.ts
+++ src/utils/repos.ts
@@ -6,6 +6,8 @@ export async function addRepository(url: `http${"s" | ""}://${string}.repo`) {
   const fileName = url.split("/").reverse().shift()!;
   const outputPath = join(getTempDir("repo", fileName), fileName);
 
+  console.log("Adding repository:", fileName);
+
   await $`
     wget -qO ${outputPath} ${url}
     install -o 0 -g 0 -m644 ${outputPath} ${`/etc/yum.repos.d/${fileName}`}
