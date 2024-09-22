843122064bd14844036205c363823b74b16effb1
Vojtěch Perník <develop@pervoj.cz>
2024-09-22 19:58:13 +0200

    feat: update package utils

--- src/utils/packages.ts
+++ src/utils/packages.ts
@@ -1,9 +1,11 @@
 import { $ } from "bun";
 
-export async function installPackages(...packages: string[]) {
+export async function installPackages(...packages: [string, ...string[]]) {
+  console.log("Installing packages:", ...packages);
   await $`rpm-ostree install ${packages}`;
 }
 
-export async function uninstallPackages(...packages: string[]) {
+export async function uninstallPackages(...packages: [string, ...string[]]) {
+  console.log("Uninstalling packages:", ...packages);
   await $`rpm-ostree uninstall ${packages}`;
 }
