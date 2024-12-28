5c5b4561a6e5da1eab81a405232ecf428e5ca56a
Vojtěch Perník <develop@pervoj.cz>
2024-12-28 18:18:19 +0100

    fix: file copying

--- src/utils/copy-files.ts
+++ src/utils/copy-files.ts
@@ -1,10 +1,10 @@
 import { $ } from "bun";
+import { readdir } from "fs/promises";
 import { join } from "path";
 
-export async function copyFiles(
-  parentDirectory: string,
-  targetDirectory = "/"
-) {
-  console.log(`Copying files: ${parentDirectory} -> ${targetDirectory}`);
-  await $`cp -r ${join(parentDirectory, "*")} ${targetDirectory}`;
+export async function copyFiles(parentDir: string, targetDir = "/") {
+  console.log(`Copying files: ${parentDir} -> ${targetDir}`);
+  const files = (await readdir(parentDir)).map((f) => join(parentDir, f));
+  await $`mkdir -p ${targetDir}`;
+  await $`cp -r ${files} ${targetDir}`;
 }
